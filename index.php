3d730911264cf63652ce839c47b16d822862a142
ianarnold <arnoldian2003@gmail.com>
2022-03-14 15:39:51 -0300

    wip

--- index.php
+++ index.php
@@ -72,7 +72,40 @@
         <div class="row bg-dark text-white">
 
             <div class="col-7">
-                
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="img/california.jpg" alt="California">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="img/dublin.jpg" alt="Dublin">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="img/paris.jpg" alt="Paris">
+                        </div>
+                    </div>
+                    <a
+                        class="carousel-control-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a
+                        class="carousel-control-next"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Próximo</span>
+                    </a>
+                </div>
             </div>
 
             <div class="col-lg-5 p-4">
