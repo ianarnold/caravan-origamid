c6066708301edd793336dc26c2f345073b5c64eb
ianarnold <arnoldian2003@gmail.com>
2022-03-15 15:06:26 -0300

    make it better

--- index.php
+++ index.php
@@ -1,19 +1,63 @@
-<!DOCTYPE html>
+<!DOCTYPE php>
 <head>
     <title>Caravan</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
+    <!-- MODAL AREA -->
+    <div
+        class="modal fade"
+        id="modalLogin"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="modalLoginTitulo"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLoginTitulo">Entre na sua conta</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="emailUser">Email</label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="emailUser"
+                                placeholder="Digite seu email...">
+                        </div>
+                        <div class="form-group">
+                            <label for="passwordUser">Senha</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="passwordUser"
+                                placeholder="Digite sua senha...">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Entrar na conta</button>
+                        <small class="form-text text-muted">Esqueceu a senha? <a href="#">Clique aqui.</a></small>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FIM MODAL AREA -->
+
+    <!-- HEADER AREA -->
     <nav
         class="navbar fixed-top navbar-expand-md navbar-light bg-light py-3 box-shadow">
-        <a href="#" class="navbar-brand">
-            <img src="img/caravan.svg" alt="Logo Caravan">
+        <a href="index.php" class="navbar-brand">
+            <img src="assets/img/caravan.svg" alt="Logo Caravan">
         </a>
         <button
             class="navbar-toggler"
@@ -39,34 +83,35 @@
                         Locais
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="local.html">California</a>
-                        <a class="dropdown-item" href="local.html">Paris</a>
-                        <a class="dropdown-item" href="local.html">Dublin</a>
+                        <a class="dropdown-item" href="pages/locais/california.php">California</a>
+                        <a class="dropdown-item" href="pages/locais/paris.php">Paris</a>
+                        <a class="dropdown-item" href="pages/locais/dublin.php">Dublin</a>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a href="planos.html" class="nav-link">
+                    <a href="pages/planos.php" class="nav-link">
                         Planos
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="contato.html" class="nav-link">
+                    <a href="pages/contato.php" class="nav-link">
                         Contato
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="" class="nav-link">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#modalLogin">
                         Login
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="" class="btn btn-outline-primary ml-md-2">
+                    <a href="#" class="btn btn-outline-primary ml-md-2">
                         Inscreva-se
                     </a>
                 </li>
             </ul>
         </div>
     </nav>
+    <!-- FIM HEADER AREA -->
 
     <!-- CARROSEL AREA -->
     <section class="container-fluid">
@@ -80,19 +125,19 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="d-block w-100" src="img/california.jpg" alt="California">
+                            <img class="d-block w-100" src="assets/img/california.jpg" alt="California">
                             <div class="carousel-caption">
                                 <h3 class="display-4">California</h3>
                             </div>
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src="img/dublin.jpg" alt="Dublin">
+                            <img class="d-block w-100" src="assets/img/dublin.jpg" alt="Dublin">
                             <div class="carousel-caption">
                                 <h3 class="display-4">Dublin</h3>
                             </div>
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src="img/paris.jpg" alt="Paris">
+                            <img class="d-block w-100" src="assets/img/paris.jpg" alt="Paris">
                             <div class="carousel-caption">
                                 <h3 class="display-4">Paris</h3>
                             </div>
@@ -143,7 +188,7 @@
             <div class="row">
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/passaporte.svg" alt="Passaporte">
+                        <img src="assets/img/icones/passaporte.svg" alt="Passaporte">
                     </div>
                     <h3>Documentos</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
@@ -151,7 +196,7 @@
                 </div>
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/passagens.svg" alt="passagens">
+                        <img src="assets/img/icones/passagens.svg" alt="passagens">
                     </div>
                     <h3>Passagens</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
@@ -159,7 +204,7 @@
                 </div>
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/translado.svg" alt="translado">
+                        <img src="assets/img/icones/translado.svg" alt="translado">
                     </div>
                     <h3>Translado</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
@@ -167,7 +212,7 @@
                 </div>
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/praias.svg" alt="praias">
+                        <img src="assets/img/icones/praias.svg" alt="praias">
                     </div>
                     <h3>Praias</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
@@ -175,7 +220,7 @@
                 </div>
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/bagagens.svg" alt="bagagens">
+                        <img src="assets/img/icones/bagagens.svg" alt="bagagens">
                     </div>
                     <h3>Bagagens</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
@@ -183,14 +228,14 @@
                 </div>
                 <div class="col-xl-4 col-md-6">
                     <div class="d-flex justify-content-center icone">
-                        <img src="img/icones/turismo.svg" alt="turismo">
+                        <img src="assets/img/icones/turismo.svg" alt="turismo">
                     </div>
                     <h3>Turismo</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in mattis
                         purus. Phasellus ullamcorper elementum ipsum.</p>
                 </div>
             </div>
-            <a href="planos.html" class="btn btn-primary btn-lg mt-4">Comprar Plano</a>
+            <a href="pages/planos.php" class="btn btn-primary btn-lg mt-4">Comprar Plano</a>
         </div>
     </section>
     <!-- FIM VANTAGENS AREA -->
@@ -209,14 +254,14 @@
     </section>
     <!-- FIM TESTIMONIAL AREA -->
 
+    <!-- DUVIDAS AREA -->
     <section class="container">
         <div class="my-5 text-center">
             <span class="h6 d-block">AINDA ESTÁ COM DÚVIDAS?</span>
             <h2 class="display-4 text-primary">Perguntas Frequentes</h2>
         </div>
         <div class="row justify-content-center">
-            <div class="col-6" id="perguntasFrequentes" data-children=".pergunta">
-
+            <div class="col-md-6 " id="perguntasFrequentes" data-children=".pergunta">
                 <div class="pergunta py-2">
                     <a
                         class="lead"
@@ -230,8 +275,6 @@
                             purus. Phasellus ullamcorper elementum ipsum.</p>
                     </div>
                 </div>
-
-
                 <div class="pergunta py-2">
                     <a
                         class="lead"
@@ -245,8 +288,6 @@
                             purus. Phasellus ullamcorper elementum ipsum.</p>
                     </div>
                 </div>
-
-
                 <div class="pergunta py-2">
                     <a
                         class="lead"
@@ -260,8 +301,6 @@
                             purus. Phasellus ullamcorper elementum ipsum.</p>
                     </div>
                 </div>
-
-
                 <div class="pergunta py-2">
                     <a
                         class="lead"
@@ -275,8 +314,6 @@
                             purus. Phasellus ullamcorper elementum ipsum.</p>
                     </div>
                 </div>
-
-
                 <div class="pergunta py-2">
                     <a
                         class="lead"
@@ -290,18 +327,83 @@
                             purus. Phasellus ullamcorper elementum ipsum.</p>
                     </div>
                 </div>
-                
             </div>
         </div>
         <div class="text-center my-4">
             <p class="small m-0">FALE CONOSCO PARA MAIS DÚVIDAS</p>
-            <a href="contato.html" class="btn btn-primary btn-lg">Entre em Contato</a>
+            <a href="pages/contato.php" class="btn btn-primary btn-lg">Entre em Contato</a>
         </div>
     </section>
+    <!-- FIM DUVIDAS AREA -->
 
-    <script type="text/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
-    <script type="text/javascript" src="js/popper.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/app.js"></script>
+    <!-- FOOTER AREA -->
+    <footer class="bg-dark text-white">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">PÁGINAS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="pages/planos.php">Planos</a>
+                        </li>
+                        <li>
+                            <a href="pages/contato.php">Contato</a>
+                        </li>
+                        <li>
+                            <a href="pages/inscricao.php">Inscreva-se</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">LOCAIS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="pages/locais/california.php">California</a>
+                        </li>
+                        <li>
+                            <a href="pages/locais/paris.php">Paris</a>
+                        </li>
+                        <li>
+                            <a href="pages/locais/dublin.php">Dublin</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 col-6">
+                    <h4 class="h6">DADOS DE CONTATO</h4>
+                    <ul class="list-unstyled text-secondary">
+                        <li>arnoldian2003@gmail.com</li>
+                        <li>(13) 4002-8922</li>
+                        <li>(13) 99715-2603</li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2 col-6">
+                    <h4 class="h6">REDES SOCIAIS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Facebook</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="bg-primary text-center py-3">
+        <p class="text-white mb-0">Caravan 2017. Alguns direitos reservados.</p>
+    </div>
+    <!-- FIM FOOTER AREA -->
+
+    <script type="text/javascript" src="assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script type="text/javascript" src="assets/js/popper.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="assets/js/app.js"></script>
 </body>
-</html>
+</php>
---
+++ pages/locais/dublin.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<head>
+    <title>Caravan - LOCAL</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+    <!-- HEADER AREA -->
+    <nav
+        class="navbar fixed-top navbar-expand-md navbar-light bg-light py-3 box-shadow">
+        <a href="../../index.php" class="navbar-brand">
+            <img src="../../assets/img/caravan.svg" alt="Logo Caravan">
+        </a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle active"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false">
+                        Locais
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="california.php">California</a>
+                        <a class="dropdown-item" href="paris.php">Paris</a>
+                        <a class="dropdown-item" href="#">Dublin</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a href="../planos.php" class="nav-link">
+                        Planos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../contato.php" class="nav-link">
+                        Contato
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="modal" data-target="#modalLogin">
+                        Login
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../inscricao.php" class="btn btn-outline-primary ml-md-2">
+                        Inscreva-se
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- FIM HEADER AREA -->
+
+    <section class="container">
+        <div class="text-center my-5">
+            <span class="h6 d-block">
+                O MELHOR DESTINO DA SUA VIAGEM
+            </span>
+            <h1 class="display-4 text-primary">Descubra Dublin</h1>
+        </div>
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- FOOTER AREA -->
+    <footer class="bg-dark text-white">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">PÁGINAS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="../planos.php">Planos</a>
+                        </li>
+                        <li>
+                            <a href="../contato.php">Contato</a>
+                        </li>
+                        <li>
+                            <a href="../inscricao.php">Inscreva-se</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">LOCAIS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="california.php">California</a>
+                        </li>
+                        <li>
+                            <a href="paris.php">Paris</a>
+                        </li>
+                        <li>
+                            <a href="#">Dublin</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 col-6">
+                    <h4 class="h6">DADOS DE CONTATO</h4>
+                    <ul class="list-unstyled text-secondary">
+                        <li>arnoldian2003@gmail.com</li>
+                        <li>(13) 4002-8922</li>
+                        <li>(13) 99715-2603</li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-6">
+                    <h4 class="h6">REDES SOCIAIS</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Facebook</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-secondary btn-block mb-2 btn-sm botaoSocial">Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="bg-primary text-center py-3">
+        <p class="text-white mb-0">Caravan 2017. Alguns direitos reservados.</p>
+    </div>
+    <!-- FIM FOOTER AREA -->
+
+    <script type="text/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
